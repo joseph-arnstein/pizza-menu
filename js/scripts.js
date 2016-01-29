51c192608371920e77951fa4698141a19070f5c1
Joey <joeyarnstein@gmail.com>
2016-01-29 11:01:10 -0800

    add front end with basic information and form, no backend for input

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,28 @@ function PizzaConstructor(){
   this.smallPrice = 13;
   this.largePrice = 18;
   this.meatToppings = [];
-}
+  this.vegetableToppings = [];
+};
 
 PizzaConstructor.prototype.meat = function(meat) {
-  this.smallPrice+= 1;
-  this.largePrice += 2;
+  this.smallPrice += 2;
+  this.largePrice += 3;
   this.meatToppings.push(meat);
-}
+};
+//PizzaConstructor.prototype.meat = function(meat) {
+//   var meats = [meat];
+//   meats.forEach(function(food) {
+//     this.smallPrice += 2;
+//     this.largePrice += 3;
+//     this.meatToppings.push(meats);
+//   });
+// };
+
+PizzaConstructor.prototype.vegetables = function (veg) {
+  this.smallPrice += 1;
+  this.largePrice += 2;
+  this.vegetableToppings.push(veg);
+};
 
 // $(document).ready(function() {
 //
--- specs/specs.js
+++ specs/specs.js
@@ -4,11 +4,20 @@ describe ("PizzaConstructor", function(){
     expect(pizza.smallPrice).to.equal(13);
     expect(pizza.largePrice).to.equal(18);
   });
-  it("add a method 'meat' for the object that adds 1 to smallPrice and 2 to largePrice, and stores meat toppings in an array for the object property 'meatToppings'", function(){
+
+  it("add a method 'meat' for the object that adds 2 to smallPrice and 3 to largePrice, and stores meat toppings in an array for the object property 'meatToppings'", function(){
     var test = new PizzaConstructor;
     test.meat("pepperoni");
-    expect(test.smallPrice).to.equal(14);
-    expect(test.largePrice).to.equal(20);
+    expect(test.smallPrice).to.equal(15);
+    expect(test.largePrice).to.equal(21);
     expect(test.meatToppings).to.eql(["pepperoni"]);
+  });
+  it("add a method 'vegetables' for the object that adds 1 to smallPrice and 2 to largePrice, and stores meat toppings in an array for the object property 'vegetableToppings'", function(){
+    var test = new PizzaConstructor;
+    test.meat();
+    test.vegetables("onion");
+    expect(test.smallPrice).to.equal(16);
+    expect(test.largePrice).to.equal(23);
+    expect(test.vegetableToppings).to.eql(["onion"]);
   });
 });
