9b93e88422d8a92dd73ddb674f91c05a47a4b56a
Joey <joeyarnstein@gmail.com>
2016-02-04 01:40:49 -0800

    refined and put more logic in backend

--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,126 @@
-var numberOfPizzas = 0
-
 function PizzaConstructor(){
-  this.size = "";
-  this.smallPrice = 13;
-  this.largePrice = 18;
-  this.meatToppings = [];
-  this.vegetableToppings = [];
+  this.size = "small";
+  this.price = 13;
+  this.toppings = [];
+};
+
+PizzaConstructor.prototype.upgradeLarge = function() {
+  this.price += 5;
+  this.size = "large";
 };
 
 PizzaConstructor.prototype.meat = function(meat) {
-  this.smallPrice += 2;
-  this.largePrice += 3;
-  this.meatToppings.push(meat);
+  this.price += 2;
+  this.toppings.push(meat);
 };
-//PizzaConstructor.prototype.meat = function(meat) {
-//   var meats = [meat];
-//   meats.forEach(function(food) {
-//     this.smallPrice += 2;
-//     this.largePrice += 3;
-//     this.meatToppings.push(meats);
-//   });
-// };
 
 PizzaConstructor.prototype.vegetables = function (veg) {
-  this.smallPrice += 1;
-  this.largePrice += 2;
-  this.vegetableToppings.push(veg);
+  this.price += 1;
+  this.toppings.push(veg);
+};
+
+
+function OrderTotal(){
+  this.pizzas = [];
+  this.totalPrice = 0
+}
+
+OrderTotal.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza)
+};
+
+OrderTotal.prototype.getTotal = function(){
+  var total = 0
+  this.pizzas.forEach(function(pizza){
+  total += pizza.price;
+  });
+  this.totalPrice = total;
 };
 
 $(document).ready(function() {
   $("#blanks form").submit(function(event){
     event.preventDefault();
     var newPizza = new PizzaConstructor;
-    newPizza.size = $("select.size").val();
+    var size = $("select.size").val();
     var meatTop = $("select.meatTop").val();
     var vegTop = $("select.vegTop").val();
-    numberOfPizzas ++;
-    if (meatTop !== "none") {
-      newPizza.meat(meatTop);
-    };
-    if (vegTop !== "none") {
-      newPizza.vegetables(vegTop);
-    };
+
+    if (size === "large"){newPizza.upgradeLarge();};
+    if (meatTop !== "none") {newPizza.meat(meatTop);};
+    if (vegTop !== "none") {newPizza.vegetables(vegTop);};
+
+    var order = new OrderTotal;
+    order.addPizza(newPizza);
+
+    // display
+
+    order.pizzas.forEach(function(pizza){
+      $("#show").append(
+        '<div class="col-sm-4">'+
+          '<h3>'+pizza.size+'</h3>'+
+            '<ul>'+
+              '<li>'+pizza.toppings[0]+'</li>'+
+              '<li>'+pizza.toppings[1]+'</li>'+
+              '<li>$'+pizza.price+'</li>'+
+            '</ul>'+
+        '</div>'
+      );
+    });
+    order.getTotal();
+    var bigPrice = order.totalPrice;
     debugger;
-
-// Create and display unordered list for pizzas & properties //
-
-
-    if (numberOfPizzas === 1){
-      $("#first-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='first-pizzaTop'></ul>");
-      if (meatTop !== "none") {
-        $("ul#first-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
-      }
-      if (vegTop !== "none"){
-        $("ul#first-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
-      }
-      if (newPizza.size === "small") {
-        $("ul#first-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
-      } else {
-        $("ul#first-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
-      }
-    } else if (numberOfPizzas === 2) {
-      $("#second-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='second-pizzaTop'></ul>");
-      if (meatTop !== "none"){
-        $("ul#second-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
-      }
-      if (vegTop !== "none"){
-        $("ul#second-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
-      }
-      if (newPizza.size === "small") {
-        $("ul#second-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
-      } else {
-        $("ul#second-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
-      }
-    }  else if (numberOfPizzas === 3) {
-      $("#third-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='third-pizzaTop'></ul>");
-      if (meatTop !== "none"){
-        $("ul#third-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
-      }
-      if (vegTop !== "none"){
-        $("ul#third-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
-      }
-      if (newPizza.size === "small") {
-        $("ul#third-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
-      } else {
-        $("ul#third-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
-      }
-    } else {
-      alert("Enough already")
-    }
+    $(".total").text("$"+bigPrice);
   });
 });
 
-// $("#add-meat").click(function(){
-// //   $("#moreMeat").append('<div class="form-group"><label for="meatTop">Meat Toppings</label><select class="form-control meatTop"><option value="pepperoni">Pepperoni</option><option value="sausage">Sausage</option><option value="bacon">Bacon</option><option value="salami">Salami</option></select></div>')
-// // })
+
+// Create and display unordered list for pizzas & properties //
+
+    // if (numberOfPizzas === 1){
+    //   $("#first-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='first-pizzaTop'></ul>");
+    //   if (meatTop !== "none") {
+    //     $("ul#first-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
+    //   }
+    //   if (vegTop !== "none"){
+    //     $("ul#first-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
+    //   }
+    //   if (newPizza.size === "small") {
+    //     $("ul#first-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
+    //   } else {
+    //     $("ul#first-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
+    //   }
+    // } else if (numberOfPizzas === 2) {
+    //   $("#second-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='second-pizzaTop'></ul>");
+    //   if (meatTop !== "none"){
+    //     $("ul#second-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
+    //   }
+    //   if (vegTop !== "none"){
+    //     $("ul#second-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
+    //   }
+    //   if (newPizza.size === "small") {
+    //     $("ul#second-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
+    //   } else {
+    //     $("ul#second-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
+    //   }
+    // }  else if (numberOfPizzas === 3) {
+    //   $("#third-pizza").append("<h3>One " + newPizza.size + " pizza</h3><ul" + " " + "id='third-pizzaTop'></ul>");
+    //   if (meatTop !== "none"){
+    //     $("ul#third-pizzaTop").append("<li>" + newPizza.meatToppings + "</li>");
+    //   }
+    //   if (vegTop !== "none"){
+    //     $("ul#third-pizzaTop").append("<li>" + newPizza.vegetableToppings + "</li>");
+    //   }
+    //   if (newPizza.size === "small") {
+    //     $("ul#third-pizzaTop").append("<li>$" + newPizza.smallPrice + "</li>")
+    //   } else {
+    //     $("ul#third-pizzaTop").append("<li>$" + newPizza.largePrice + "</li>")
+    //   }
+    // } else {
+    //   alert("Enough already")
+    // }
+  //});
+//});
+//
+// // $("#add-meat").click(function(){
+// // //   $("#moreMeat").append('<div class="form-group"><label for="meatTop">Meat Toppings</label><select class="form-control meatTop"><option value="pepperoni">Pepperoni</option><option value="sausage">Sausage</option><option value="bacon">Bacon</option><option value="salami">Salami</option></select></div>')
+// // // })
--- specs/specs.js
+++ specs/specs.js
@@ -1,23 +1,48 @@
 describe ("PizzaConstructor", function(){
-  it("creates an object with a default price of for small pizza of 13, and 18 for a large", function(){
+  it("creates an object with a default price of for small pizza of 13", function(){
     var pizza = new PizzaConstructor;
-    expect(pizza.smallPrice).to.equal(13);
-    expect(pizza.largePrice).to.equal(18);
+    expect(pizza.price).to.equal(13);
   });
 
-  it("add a method 'meat' for the object that adds 2 to smallPrice and 3 to largePrice, and stores meat toppings in an array for the object property 'meatToppings'", function(){
-    var test = new PizzaConstructor;
-    test.meat("pepperoni");
-    expect(test.smallPrice).to.equal(15);
-    expect(test.largePrice).to.equal(21);
-    expect(test.meatToppings).to.eql(["pepperoni"]);
+  it("add a method 'upgradeLarge' for the object that adds 5 to price and change the property 'size' to equal 'large'", function(){
+    var pizza = new PizzaConstructor;
+    pizza.upgradeLarge();
+    expect(pizza.price).to.equal(18);
+    expect(pizza.size).to.equal("large");
   });
-  it("add a method 'vegetables' for the object that adds 1 to smallPrice and 2 to largePrice, and stores meat toppings in an array for the object property 'vegetableToppings'", function(){
-    var test = new PizzaConstructor;
-    test.meat();
-    test.vegetables("onion");
-    expect(test.smallPrice).to.equal(16);
-    expect(test.largePrice).to.equal(23);
-    expect(test.vegetableToppings).to.eql(["onion"]);
+
+  it("add a method 'meat' for the object that adds 2 to price, and stores meat toppings in an array for the object property 'toppings'", function(){
+    var pizza = new PizzaConstructor;
+    pizza.meat("pepperoni");
+    expect(pizza.price).to.equal(15);
+    expect(pizza.toppings).to.eql(["pepperoni"]);
+  });
+
+  it("add a method 'vegetables' for the object that adds 1 to price, and stores meat toppings in an array for the object property 'toppings'", function(){
+    var pizza = new PizzaConstructor;
+    pizza.vegetables("onion");
+    expect(pizza.price).to.equal(14);
+    expect(pizza.toppings).to.eql(["onion"]);
+  });
+});
+
+describe("OrderTotal", function(){
+  it("will add pizzas as objects into an array", function(){
+    var pizza = new PizzaConstructor;
+    var pizza2 = new PizzaConstructor;
+    var order = new OrderTotal;
+    order.addPizza(pizza);
+    order.addPizza(pizza2);
+    expect(order.pizzas).to.eql([pizza, pizza2]);
+  });
+  it("will return total order price", function(){
+    var test1 = new PizzaConstructor;
+    test1.meat("pepperoni");
+    var test2 = new PizzaConstructor;
+    var order = new OrderTotal;
+    order.addPizza(test1);
+    order.addPizza(test2);
+    order.getTotal();
+    expect(order.totalPrice).to.equal(28);
   });
 });
